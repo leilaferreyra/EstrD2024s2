8ddba60ad2f54e99bbdba716057118a22a956ba8
leilaferreyra <ferreyraleila0@gmail.com>
2024-09-05 19:04:02 -0300

    falta ultimo punto

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -23,10 +23,6 @@ esMismoColor     Azul    Azul  =  True
 esMismoColor     Rojo    Rojo  =  True
 esMismoColor     _       _     =  False 
 
---colorDeBolita :: Celda -> Color 
---colorDeBolita (CeldaVacia)     = error "Una celda vacia no tiene color"
---colorDeBolita (Bolita color _) = color
-
 unoSi :: Bool -> Int  
 unoSi    True = 1 
 unoSi    False= 0
@@ -42,9 +38,9 @@ esCeldaVacia    _          = False
 --Gobstones, esta función es total.
 sacar :: Color -> Celda           -> Celda
 sacar    _        CeldaVacia       = CeldaVacia
-sacar    color    (Bolita c celda) = if esMismoColor color c 
+sacar    c1    (Bolita c2 celda) = if esMismoColor c1 c2 
                                      then celda 
-                                     else Bolita c (sacar color celda)
+                                     else Bolita c2 (sacar c1 celda)
 
 --Dado un número n, un color c, y una celda, agrega n bolitas de color c a la celda.
 ponerN :: Int -> Color -> Celda -> Celda
@@ -123,9 +119,11 @@ data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
 --dena las siguientes funciones utilizando recursión estructural según corresponda:
 --1.
 a1::Tree Int 
-a1 = NodeT 1 (NodeT 3 EmptyT EmptyT) (NodeT 3 EmptyT EmptyT)
+a1 = NodeT 1 (NodeT 2 EmptyT EmptyT)(NodeT 3 (NodeT 4 EmptyT EmptyT) EmptyT)
 a2 :: Tree Int 
 a2 = NodeT 3 (EmptyT) (EmptyT)
+a3:: Tree Int 
+a3 = NodeT 1 (NodeT 2 (NodeT 3 EmptyT EmptyT) EmptyT)(NodeT 5 (NodeT 6 EmptyT EmptyT) (NodeT 7 (NodeT 9 EmptyT EmptyT) EmptyT))
 sumarT :: Tree Int -> Int
 sumarT EmptyT = 0
 sumarT (NodeT x d i) = x + sumarT d + sumarT i
@@ -161,3 +159,76 @@ leaves    (NodeT x EmptyT EmptyT) = [x]
 leaves    (NodeT x d i) = (leaves d) ++ (leaves i)
 --Dado un árbol devuelve los elementos que se encuentran en sus hojas.
 --NOTA: en este tipo se dene como hoja a un nodo con dos hijos vacíos.
+
+--7.
+heightT :: Tree a -> Int
+heightT EmptyT = 0 
+heightT (NodeT x i d) =  1 + max (heightT i) (heightT d)
+
+--Dado un árbol devuelve su altura.
+--Nota: la altura de un árbol (height en inglés), también llamada profundidad, es
+--la cantidad de niveles del árbol1
+--La altura para EmptyT es 0, y para una hoja
+--es 1.
+--8.
+mirrorT :: Tree a -> Tree a
+mirrorT EmptyT = EmptyT
+mirrorT (NodeT x i d) = NodeT x (mirrorT d) (mirrorT i) 
+--Dado un árbol devuelve el árbol resultante de intercambiar el hijo izquierdo con
+--el derecho, en cada nodo del árbol.
+--9. 
+toList :: Tree a -> [a]
+toList EmptyT = []
+toList (NodeT x i d) = toList i ++ [x] ++ toList d
+--Dado un árbol devuelve una lista que representa el resultado de recorrerlo en
+--modo in-order.
+--Nota: En el modo in-order primero se procesan los elementos del hijo izquierdo,
+--luego la raiz y luego los elementos del hijo derecho.
+--10. 
+levelN :: Int -> Tree a -> [a]
+levelN    n      EmptyT = []
+levelN    0      (NodeT x _ _ ) = [x]
+levelN     n      (NodeT _ i d) =  levelN (n-1) i ++ levelN (n-1) d
+--Dados un número n y un árbol devuelve una lista con los nodos de nivel n. El
+--nivel de un nodo es la distancia que hay de la raíz hasta él. La distancia de la
+--raiz a sí misma es 0, y la distancia de la raiz a uno de sus hijos es 1.
+--Nota: El primer nivel de un árbol (su raíz) es 0.
+--11.
+listPerLevel :: Tree a -> [[a]]
+listPerLevel EmptyT          = []
+listPerLevel (NodeT x t1 t2) = [x] : juntarNiveles (listPerLevel t1) (listPerLevel t2)
+
+juntarNiveles :: [[a]] -> [[a]] -> [[a]]
+juntarNiveles []       yss      = yss
+juntarNiveles xss      []       = xss
+juntarNiveles (xs:xss) (ys:yss) = (xs ++ ys) : juntarNiveles xss yss
+
+
+--Dado un árbol devuelve una lista de listas en la que cada elemento representa
+--un nivel de dicho árbol.
+--12. 
+ramaMasLarga :: Tree a -> [a]
+ramaMasLarga EmptyT = []
+ramaMasLarga (NodeT x i d ) =  if (heightT i > heightT d)
+                              then x : ramaMasLarga i
+                              else x : ramaMasLarga d
+--Devuelve los elementos de la rama más larga del árbol
+--13. 
+todosLosCaminos :: Tree a -> [[a]]
+todosLosCaminos EmptyT          = []
+todosLosCaminos (NodeT x t1 t2) = [x] : consACada x (todosLosCaminos t1) ++ consACada x (todosLosCaminos t2)
+
+consACada :: a -> [[a]] -> [[a]]
+consACada x []       = []
+consACada x (xs:xss) = (x:xs) : consACada x xss 
+--Dado un árbol devuelve todos los caminos, es decir, los caminos desde la raíz
+--hasta cualquiera de los nodos.
+--ATENCIÓN: se trata de todos los caminos, y no solamente de los maximales (o
+--sea, de la raíz hasta la hoja), o sea, por ejemplo
+--todosLosCaminos (NodeT 1 (NodeT 2 (NodeT 3 EmptyT EmptyT)
+--EmptyT)
+--(NodeT 4 (NodeT 5 EmptyT EmptyT)
+--EmptyT))
+-- = [ [1], [1,2], [1,2,3], [1,4], [1,4,5] ]
+--OBSERVACIÓN: puede resultar interesante plantear otra función, variación de
+--ésta para devolver solamente los caminos maximales.
